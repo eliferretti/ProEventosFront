496fec16dedb2826a0446e000910e8f39c10c386
eliferretti <eliferretti@gmail.com>
2023-03-20 19:42:37 -0300

    107

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,9 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { TituloComponent } from './shared/titulo/titulo.component';
 import { EventoDetalheComponent } from './components/eventos/evento-detalhe/evento-detalhe.component';
 import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
+import { UserComponent } from './components/user/user.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
 
 
 @NgModule({
@@ -41,7 +44,10 @@ import { EventoListaComponent } from './components/eventos/evento-lista/evento-l
     TituloComponent,
     DateTimeFormatPipe,
     EventoDetalheComponent,
-    EventoListaComponent
+    EventoListaComponent,
+    UserComponent,
+    LoginComponent,
+    RegistrationComponent
    ],
   imports: [
     BrowserModule,
